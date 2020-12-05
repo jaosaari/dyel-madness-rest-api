5b0059be6f37eeb4c1d4aeb44195a39fd38cd920
jaosaari <jaosaari@gmail.com>
2020-12-05 09:41:17 +0100

    build added

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require("express")
-const cors = require("cors")
-
 const app = express()
+
+const cors = require("cors")
 
 app.use(cors())
 app.use(express.json())
-
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method)
-  console.log("Path:  ", request.path)
-  console.log("Body:  ", request.body)
-  console.log("---")
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" })
-}
-
-app.use(requestLogger)
+app.use(express.static("build"))
 
 let notes = [
   {
@@ -40,10 +27,6 @@ let notes = [
     important: true,
   },
 ]
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>")
-})
 
 app.get("/api/notes", (req, res) => {
   res.json(notes)
@@ -92,9 +75,8 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end()
 })
 
-app.use(unknownEndpoint)
-
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
